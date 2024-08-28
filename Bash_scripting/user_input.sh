9301966a850c97802b54b86bdb09dcf2a2797726
tomcatprime <admin@cloudpoweredlearning.com>
2024-08-28 17:43:05 +0300

    user input

---
+++ Bash_scripting/user_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter your skills:"
+read SKILL
+
+echo "Your $SKILL is in high Demand in the IT industry"
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo "#######loadling#########"
+
+echo "Login Successful: Welcome user $USR"
