1a9943001f7a608e7396605c87feccc2aa50598e
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-08-29 10:55:58 +0300

    Create for_loop.sh

---
+++ Bash_scripting/for_loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for VAR1 in java .net python ruby php
+do
+  echo "Loooping...."
+  sleep 2
+  echo "####################################"
+  echo "Value of Var1 is $VAR1."
+  echo "####################################"
+  sleep 2
+  date
+done
+  
