0341ed5200d31794e4484b398ddf7307c216da23
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-08-29 11:09:41 +0300

    Update while_loop.sh

--- Bash_scripting/while_loop.sh
+++ Bash_scripting/while_loop.sh
@@ -6,5 +6,8 @@ while [ $counter -lt 5 ]
 do
   echo "Loooping....."
   echo "Value of counter is $counter."
-  counter=$(($counter + 1))
+  counter=$(( $counter + 1 ))
+  sleep 2
 done
+
+echo "Out of the loop"
